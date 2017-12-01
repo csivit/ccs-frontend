8ef978f1d1a9b1f1a20d2bb5f3cc1fd4d6b38c33
callmetarush <tarush.nagpal7@gmail.com>
2017-12-01 13:12:00 +0530

    fixed double

--- assets/js/register.js
+++ assets/js/register.js
@@ -113,17 +113,7 @@ $(document).ready(function () {
 
             // Prevent form submission
             e.preventDefault();
-
-            // Get the form instance
-            var $form = $(e.target);
-
-            // Get the BootstrapValidator instance
-            var bv = $form.data('bootstrapValidator');
-
-            // Use Ajax to submit form data
-            $.post($form.attr('action'), $form.serialize(), function(result) {
-                console.log(result);
-            }, 'json');
+;
         });
 
 });
